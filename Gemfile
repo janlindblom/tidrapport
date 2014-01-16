0c48ec30843586c6bd764d5788d31e9ab809e696
Jan Lindblom <jan.lindblom@mittmedia.se>
2014-01-16 11:09:16 +0100

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,35 @@
+source 'https://rubygems.org'
+
+ruby '2.0.0'
+
+gem 'rails', '4.0.2'
+
+gem 'mysql2'
+
+gem 'sass-rails', '~> 4.0.0'
+
+gem 'uglifier', '>= 1.3.0'
+
+gem 'coffee-rails', '~> 4.0.0'
+
+gem 'therubyracer', platforms: :ruby
+
+gem 'jquery-rails'
+
+gem 'foundation-rails'
+
+gem 'turbolinks'
+
+gem 'jbuilder', '~> 1.2'
+
+group :doc do
+  gem 'sdoc', require: false
+end
+
+gem 'bcrypt-ruby', '~> 3.1.2'
+
+gem 'unicorn'
+
+gem 'capistrano', group: :development
+
+gem 'debugger', group: [:development, :test]
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tidrapport::Application.config.secret_key_base = '00dc8a64ba62dc8c86179ee4ec3e870f0bea2f3e252ba3937d2d48ef769ce07bbe65ae039fee0e6737bfc3987f1b0dad30ba2106bb2028000e25a239b0e691ca'
